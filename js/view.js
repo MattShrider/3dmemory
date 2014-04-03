9bac4f347a30911105e82097e897db08982b2dc8
MattShrider <mattshrider@gmail.com>
2014-04-03 12:47:15 -0400

    Updated GUI to add colors and headers. Doc update.

--- js/view.js
+++ js/view.js
@@ -1,5 +1,22 @@
+/** 
+ * @file js/view.js This file contains the functions for adding and removing from
+ * the front-facing website.
+ * @author Matthew Shrider <mattshrider@gmail.com>
+ */
+
+/** 
+* View namespace for wrapping up GUI helpers/functions.
+* @namespace view
+*/
 var view = {
+   /**
+    * @method addProc
+    * Adds a process to the GUI
+    * @param {Proc} p The process object containg all of the data to add to the GUI
+    * @memberof view
+    */
    addProc: function(p){
+      /* Find a random color and convert it to a HEX code */
       var color = Math.floor((Math.random() * 0.5 + 0.5)*16777215).toString(16);
       var $proc = $('<li id="pid' + p.id + '"class="block" style="background: #' + color + '">' +
                     '<span class="center">Proc ID: ' + p.id + '</span>' +
@@ -11,7 +28,7 @@ var view = {
                     '</ul>' +
                     '</div>' +
                     '</li>');
-      $('#procs').append($proc);
+      $('#procs').append($proc.hide().fadeIn(300));
 
       var $text;
       var $mem;
@@ -28,19 +45,31 @@ var view = {
          } else {
             $text = $proc.find('.data');
          }
-         $text.append($('<li>' + i + ' '+ type +'  <span class="pageframe">Frame: ' + p.pageTable[i].frame + '</span></li>'));
+         $text.append($('<li>' + i + ' '+ type +'  <span class="pageframe">Frame: ' + p.pageTable[i].frame + '</span></li>').hide().fadeIn(500));
 
          /* Add info to memory */
          $mem = $('#memory #f' + p.pageTable[i].frame);
          $mem.css("background", "#"+color);
-         $frametext = $('<div class="memtext">PID: ' + p.id + '  Type: ' + type + '  Page: ' + i + '</div>');
+         var textcolor;
+         if (type === 'text'){
+            textcolor="#FEEBE1";
+         } else {
+            textcolor="#E1FEFC";
+         }
+         $frametext = $('<div style="background: '+textcolor+'" class="memtext">PID: ' + p.id + '  Page: ' + i + '  Type: ' + type + '</div>');
 
-         $mem.append($frametext);
+         $mem.append($frametext.hide().fadeIn(300));
       }
    },
 
+   /**
+    * @method removeProc
+    * Removes a process from the GUI
+    * @param {Proc} p The process object containg all of the data to remove from the GUI
+    * @memberof view
+    */
    removeProc: function(p){
-      var $proc = $('#procs #pid' + p.id).remove();
+      $('#procs #pid' + p.id).fadeOut(200, function(){this.remove();});
       var $frame;
       for (var i=0; i < p.pageTable.length; i++){
          $('#memory #f' + p.pageTable[i].frame).css("background", "#abcdef");
@@ -48,20 +77,21 @@ var view = {
       }
    },
 
-   movePage: function(p){
-
-   },
-
+   /**
+    * @method freeFrame
+    * Adds new free frames to the Free Frame list
+    * @param {Number} f Which frame was just freed
+    * @memberof view
+    */
    freeFrame: function(f){
       var $free = $('#free');
       var $frame = $('<li id="free' + f + '">' + f + '</li>');
       $free.prepend($frame);
    },
 
-   unFreeFrame: function(f, page, proc){
-   },
 };
 
+/* Will be ran when the site has loaded, useful for animations */
 $(document).ready(function(){
 
 });
